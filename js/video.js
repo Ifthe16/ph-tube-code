49b6e5401460a2c4a95782358d23d608fa363768
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-07 11:36:13 +0600

    added profile picture, icon, name dynamically inside card section

--- js/video.js
+++ js/video.js
@@ -48,16 +48,22 @@ const displayVideos = (videos) => {
         const card = document.createElement("div");
         card.classList = "card card-compact";
         card.innerHTML = `
-         <figure>
+         <figure class="h-[200px]">
     <img
-      src=${video.thumbnail} />
+      src=${video.thumbnail} class="h-full w-full object-cover" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>${video.description}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+  <div class="px-0 py-2 flex gap-2">
+   <div>
+   <img class="w-10 h-10 rounded-full object-cover" src= ${video.authors[0].profile_picture}/>
+   </div>
+   <div>
+   <h2 class="font-bold">${video.title}</h2>
+  <div class="flex items-center gap-2">
+  <p class="text-gray-400">${video.authors[0].profile_name}</p>
+  <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />
+  </div>
+   <p> </p>
+   </div>
   </div>
         `;
         videoContainer.append(card);
