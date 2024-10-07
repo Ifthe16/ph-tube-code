f881114f4a48f08221cea306e4d706f195aec2c8
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-07 17:03:44 +0600

    added render conditional on verified

--- js/video.js
+++ js/video.js
@@ -48,10 +48,11 @@ const displayVideos = (videos) => {
         const card = document.createElement("div");
         card.classList = "card card-compact";
         card.innerHTML = `
-         <figure class="h-[200px]">
+         <figure class="h-[200px] relative">
     <img
       src=${video.thumbnail} class="h-full w-full object-cover" />
-  </figure>
+      <span class="absolute right-2 bottom-8 bg-black rounded p-1 text-white">${video.others.posted_date}</span>
+  </figure>F
   <div class="px-0 py-2 flex gap-2">
    <div>
    <img class="w-10 h-10 rounded-full object-cover" src= ${video.authors[0].profile_picture}/>
@@ -60,7 +61,7 @@ const displayVideos = (videos) => {
    <h2 class="font-bold">${video.title}</h2>
   <div class="flex items-center gap-2">
   <p class="text-gray-400">${video.authors[0].profile_name}</p>
-  <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />
+  ${video.authors[0].verified === true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" /> `: ""}
   </div>
    <p> </p>
    </div>
