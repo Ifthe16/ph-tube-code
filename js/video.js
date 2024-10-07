71900fcff4d6543feaeba1bdf6cdb4e123a6664d
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-07 09:32:06 +0600

    added video container section dynamically

--- js/video.js
+++ js/video.js
@@ -12,9 +12,20 @@ const loadCategories = () => {
         .catch((error) => console.log(error));
 };
 
+// create loadVideos
+const loadVideos = () => {
+
+    // fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((res) => res.json())
+        // .then((data) => console.log(data.categories))
+        .then((data) => displayVideos(data.videos))
+        .catch((error) => console.log(error));
+};
+
 // create displayCategories
 const displayCategories = (categories) => {
-const categoryContainer = document.getElementById('categories');
+    const categoryContainer = document.getElementById('categories');
 
     categories.forEach((item) => {
         console.log(item);
@@ -28,5 +39,29 @@ const categoryContainer = document.getElementById('categories');
         categoryContainer.append(button);
     });
 };
-loadCategories();
 
+// create displayVideos
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos');
+    videos.forEach(video => {
+        console.log(video);
+        const card = document.createElement("div");
+        card.classList = "card card-compact";
+        card.innerHTML = `
+         <figure>
+    <img
+      src=${video.thumbnail} />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        `;
+        videoContainer.append(card);
+    });
+}
+loadCategories();
+loadVideos();
