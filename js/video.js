4e26263ee97bec76b440ce3f17c089a1a1876cc7
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-07 22:34:19 +0600

    implemented search functionally

--- js/video.js
+++ js/video.js
@@ -31,10 +31,10 @@ const loadCategories = () => {
 };
 
 // create loadVideos
-const loadVideos = () => {
+const loadVideos = (searchText = "") => {
 
     // fetch the data
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
         .then((res) => res.json())
         // .then((data) => console.log(data.categories))
         .then((data) => displayVideos(data.videos))
@@ -146,5 +146,11 @@ const displayVideos = (videos) => {
         videoContainer.append(card);
     });
 }
+
+document.getElementById('search-input').addEventListener("keyup", (e) => {
+    // console.log(e.target.value);
+    loadVideos(e.target.value);
+});
+
 loadCategories();
 loadVideos();
