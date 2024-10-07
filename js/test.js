2f0c2520150a35e29b8ea4021debf45d45ec34d9
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-07 17:43:51 +0600

    added conditional time and formated time

--- js/test.js
+++ js/test.js
@@ -8,3 +8,13 @@ else {
 }
 
 console.log(`${isVerified === true ? "user is verified" : "user is not verified"}`);
+
+function getTimeString(time) {
+    // get Hour and rest seconds
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+console.log(getTimeString(7865));
--- js/video.js
+++ js/video.js
@@ -1,4 +1,14 @@
 console.log('video script added');
+
+function getTimeString(time) {
+    // get Hour and rest seconds
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+
 // 1 - Fetch, Load and Show Categories on html
 
 // create loadCategories
@@ -51,8 +61,10 @@ const displayVideos = (videos) => {
          <figure class="h-[200px] relative">
     <img
       src=${video.thumbnail} class="h-full w-full object-cover" />
-      <span class="absolute right-2 bottom-8 bg-black rounded p-1 text-white">${video.others.posted_date}</span>
-  </figure>F
+      ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-8 bg-black rounded p-1 text-white">${getTimeString(video.others.posted_date)}</span>`
+            }
+      
+  </figure>
   <div class="px-0 py-2 flex gap-2">
    <div>
    <img class="w-10 h-10 rounded-full object-cover" src= ${video.authors[0].profile_picture}/>
@@ -61,7 +73,7 @@ const displayVideos = (videos) => {
    <h2 class="font-bold">${video.title}</h2>
   <div class="flex items-center gap-2">
   <p class="text-gray-400">${video.authors[0].profile_name}</p>
-  ${video.authors[0].verified === true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" /> `: ""}
+  ${video.authors[0].verified === true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" /> ` : ""}
   </div>
    <p> </p>
    </div>
